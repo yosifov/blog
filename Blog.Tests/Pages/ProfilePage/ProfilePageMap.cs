e1f1a9ca3902ab2990eff5f74202a3c1f6c9c2c2
Yosifov <k.yosifov@gmail.com>
2019-03-14 00:32:27 +0200

    Add Profile Page Message Elements for Blog Tests Project

--- Blog.Tests/Pages/ProfilePage/ProfilePageMap.cs
+++ Blog.Tests/Pages/ProfilePage/ProfilePageMap.cs
@@ -13,5 +13,9 @@
         public IWebElement ConfirmPassword => Wait.Until(d => d.FindElement(By.Id("ConfirmPassword")));
 
         public IWebElement SubmitButton => Wait.Until(d => d.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[5]/div/input")));
+
+        public IWebElement SuccessMsg => Wait.Until(d => d.FindElement(By.ClassName("text-success")));
+
+        public IWebElement ErrorMsg => Wait.Until(d => d.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[1]/ul/li")));
     }
 }
