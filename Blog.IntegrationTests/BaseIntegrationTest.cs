3b6d27a079f956839ea3191aa1ac7f4ed424cab1
Lili Nikolova <lili.nikoll@gmail.com>
2019-03-29 13:55:41 +0200

    Integration Tests

---
+++ Blog.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Blog.IntegrationTests
+{
+    using AutoFixture;
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+    using System.Net.Http;
+
+    public class BaseIntegrationTest
+    {
+        public HttpClient Client { get; set; }
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:60634");
+
+        }
+
+        public Fixture Fixture => new Fixture();
+
+       
+    }
+}
+
---
+++ Blog.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,53 @@
+﻿namespace IntegrationTests
+{
+    using Blog.IntegrationTests;
+    using NUnit.Framework;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class IntegrationTests : BaseIntegrationTest
+    {
+
+        [Test]
+        public async Task GetOkStatusCode()
+        {
+            var response = await Client.GetAsync("/Article/List");
+
+            response.EnsureSuccessStatusCode();
+            var responseAsString = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Test]
+        public async Task RenderRegistrationForm()
+        {
+            var response = await Client.GetAsync("/Account/Register");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseAsString = await response.Content.ReadAsStringAsync();
+
+            Assert.That(responseAsString.Contains("Register"));
+
+        }
+
+        [Test]
+        public async Task GetASingleArticle()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/Article/Details/5");
+            var path = Path.GetFullPath(Directory.GetCurrentDirectory() + "/../../../Article.json");
+
+
+            var response = await Client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var responseAsString = await response.Content.ReadAsStringAsync();
+
+            Assert.That(responseAsString.Contains("Dulcy"));
+
+        }
+    }
+}
+
